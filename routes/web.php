19e997ff8c343be19d2a76f85615fbab41f23e4c
Nicolás Ignacio Villar <villarnicolasignacio@gmail.com>
2022-04-29 15:13:03 -0300

    primera subida

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource('libros', App\Http\Controllers\LibroController::class)->middleware('auth');
+Route::resource('categorias', App\Http\Controllers\CategoriaController::class)->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
