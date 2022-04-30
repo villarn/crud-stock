5d337944cc3a5be46db4910c919e5740b1754892
Nicolás Ignacio Villar <villarnicolasignacio@gmail.com>
2022-04-30 10:46:49 -0300

    d

--- resources/views/libro/form.blade.php
+++ resources/views/libro/form.blade.php
@@ -2,17 +2,17 @@
     <div class="box-body">
         <div class="form-group">
             {{ Form::label('categoria_id') }}
-            {{ Form::select('categoria_id', $categoria, $libro->categoria_id, ['class' => 'form-control' . ($errors->has('categoria_id') ? ' is-invalid' : ''), 'placeholder' => 'Categoria']) }}
+            {{ Form::select('categoria_id', $categoria, $libro->categoria_id, ['class' => 'form-control' . ($errors->has('categoria_id') ? ' is-invalid' : ''), 'placeholder' => 'Seleccionar la categoría']) }}
             {!! $errors->first('categoria_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('nombre') }}
-            {{ Form::text('nombre', $libro->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {{ Form::text('nombre', $libro->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Ingrese el nombre']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>        
         <div class="form-group">
-            {{ Form::label('Cantidad') }}
-            {{ Form::text('stock', $libro->stock, ['class' => 'form-control' . ($errors->has('stock') ? ' is-invalid' : ''), 'placeholder' => 'Cantidad']) }}
+            {{ Form::label('stock') }}
+            {{ Form::text('stock', $libro->stock, ['class' => 'form-control' . ($errors->has('stock') ? ' is-invalid' : ''), 'placeholder' => 'ingrese el stock']) }}
             {!! $errors->first('stock', '<div class="invalid-feedback">:message</div>') !!}
         </div>
     </div>
