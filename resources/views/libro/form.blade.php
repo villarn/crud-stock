a682ace5dc1799eb0972601701364569908eba01
Nicolás Ignacio Villar <villarnicolasignacio@gmail.com>
2022-04-30 10:32:30 -0300

    m

--- resources/views/libro/form.blade.php
+++ resources/views/libro/form.blade.php
@@ -1,6 +1,5 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-        
         <div class="form-group">
             {{ Form::label('categoria_id') }}
             {{ Form::select('categoria_id', $categoria, $libro->categoria_id, ['class' => 'form-control' . ($errors->has('categoria_id') ? ' is-invalid' : ''), 'placeholder' => 'Categoria']) }}
@@ -10,8 +9,12 @@
             {{ Form::label('nombre') }}
             {{ Form::text('nombre', $libro->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>        
+        <div class="form-group">
+            {{ Form::label('Cantidad') }}
+            {{ Form::text('stock', $libro->stock, ['class' => 'form-control' . ($errors->has('stock') ? ' is-invalid' : ''), 'placeholder' => 'Cantidad']) }}
+            {!! $errors->first('stock', '<div class="invalid-feedback">:message</div>') !!}
         </div>
-        
     </div>
     <div class="box-footer mt20">
         <button type="submit" class="btn btn-primary">Submit</button>
